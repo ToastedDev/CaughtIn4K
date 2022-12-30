a0bd4909721af7204127e3c42a96df7300de31e0
Not <realtcatech@gmail.com>
2022-12-30 05:47:50 +0000

    add basic client creation

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv/config");
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.login(process.env.TOKEN);
